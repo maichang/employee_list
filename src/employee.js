3e9d20862541e577359b36061f8f10168106acc9
maichang <mai.niikura@gmail.com>
2020-01-20 13:42:49 +0900

    Revert "社員登録アプリ"
    This reverts commit 5ac8047580acfc75b736c88f3009e132b7ae987d.

--- src/employee.js
+++
@@ -1,8 +0,0 @@
-export default class Employee {
-    constructor(id, name, birthday, salary) {
-        this.id = id
-        this.name = name
-        this.birthday = birthday
-        this.salary = salary
-    }
-  }
