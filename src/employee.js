5ac8047580acfc75b736c88f3009e132b7ae987d
maichang <mai.niikura@gmail.com>
2020-01-20 13:31:42 +0900

    社員登録アプリ

---
+++ src/employee.js
@@ -0,0 +1,8 @@
+export default class Employee {
+    constructor(id, name, birthday, salary) {
+        this.id = id
+        this.name = name
+        this.birthday = birthday
+        this.salary = salary
+    }
+  }
